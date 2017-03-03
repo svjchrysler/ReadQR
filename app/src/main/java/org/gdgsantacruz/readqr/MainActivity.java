dcda35ebbbefd5f00aa8c29b43af1295638bfaa1
Jhon Salguero <svjchrysler@gmail.com>
2017-03-03 14:40:45 -0400

    first commit

---
+++ app/src/main/java/org/gdgsantacruz/readqr/MainActivity.java
@@ -0,0 +1,42 @@
+package org.gdgsantacruz.readqr;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class MainActivity extends Activity implements ZXingScannerView.ResultHandler {
+
+    private ZXingScannerView mScannerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mScannerView = new ZXingScannerView(this);
+        setContentView(mScannerView);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mScannerView.setResultHandler(this);
+        mScannerView.startCamera();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mScannerView.stopCamera();
+    }
+
+    @Override
+    public void handleResult(Result result) {
+        Log.e("Error", result.getText().toString());
+        Toast.makeText(this, result.getText().toString(), Toast.LENGTH_SHORT).show();
+        mScannerView.resumeCameraPreview(this);
+    }
+}
